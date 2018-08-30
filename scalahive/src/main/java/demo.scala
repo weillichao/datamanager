583ab477c0698a4fe95bf5994af7d7236259d7bc
weilichao <weilichao@sczq.com.cn>
2018-08-30 10:51:57 +0800

    ipdate# Please enter the commit message for your changes. Lines starting

---
+++ scalahive/src/main/java/demo.scala
@@ -0,0 +1,37 @@
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
+
+
+object demo {
+  def main(args: Array[String]): Unit = {
+
+
+    val spark = SparkSession
+      .builder()
+      .enableHiveSupport()
+      .appName("Spark Hive")
+      .master("local[2]")
+      .getOrCreate()
+
+    spark.sql("show tables").rdd.foreach(row => {
+      println("row.size:" + row.size + "  " + row.getString(1))
+    })
+    var sqlrdd=spark.sql("show tables");
+   sqlrdd.rdd.flatMap{name=>name.toString().split(",")}.foreach(row => {
+      println(row.trim.toString)
+    })
+  }
+
+  def mapTest() {
+    val conf = new SparkConf()
+      .setAppName("map")
+      .setMaster("local")
+    val sc = new SparkContext(conf)
+    val numbers = Array(1, 2, 3, 4, 5)
+    val numberRDD = sc.parallelize(numbers, 1)
+    val multipleNumberRDD = numberRDD.map { number => number * 3 }
+    multipleNumberRDD.foreach { num => println(num) }
+
+  }
+}
